42fb0f880b5690231698e762f49f2cb820e4881a
shashi <shashi@Shashikants-MacBook-Air.local>
2025-03-12 17:06:18 +0530

    btn css

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -4,15 +4,20 @@ export default function decorate(block) {
   const label = rows[1]?.textContent.trim() || 'Default Button';
   const link = rows[3]?.textContent.trim() || '#';
   const style = rows[5]?.textContent.trim() || 'default';
+  const title = rows[7]?.textContent.trim() || 'default';
+  const arialabel = rows[9]?.textContent.trim() || 'default';
 
   // Clear the block’s original content
   block.innerHTML = '';
 
   // Create the button element
   const button = document.createElement('a');
-  button.classList.add('btn', style);
+  button.className = 'btn ' + style;
   button.href = link;
+  button.title = title;
+  button.ariaLabel = arialabel;
   button.textContent = label;
+  console.log(style);
   // Append to block
   block.append(button);
 }
